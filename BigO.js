450c6173e09b3b6d9ffe87d36e13a2e4c5e057c4
Ricky27 <Rickyrenteria7@gmail.com>
2022-03-11 10:13:48 -0800

    VS Notes Commit and Push

---
+++ BigO.js
@@ -0,0 +1,21 @@
+// //**MEASURE PERFORMANCE OF ALGORITHEMS (TIME COMPLEXITY - BIG O)**
+
+// //-------------------------------------------------------//
+
+// //use devtools for this algorithem//
+// //**LINEAR TIME FUNC** (INCREASES INPUT = INCREASE POERFORMANCE IN A LINEAR WAY)
+// function sumUp(n) {
+//     let result = 0;
+//     for (let i = 1; i <= n; i++) {
+//         result += i;
+//     }
+//     return result;
+//     //TO SEE ANSWER USE *SUMUP('NUMBER')*
+// };
+// //
+
+// //**CONSTANT TIME FUNC**(NUMBER OF INPUTS DOESNT AFFECT PERFORMANCE)
+
+// function sumUp(n) {
+//     return (n / 2) * (1 + n);
+// };
